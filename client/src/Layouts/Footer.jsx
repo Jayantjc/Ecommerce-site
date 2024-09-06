808b4131e3b5480f8ea14d455e6fb7da0ea96e12
Jayant Chugh <108174076+Jayantjc@users.noreply.github.com>
2024-09-07 00:14:10 +0530

    Update Footer.jsx

--- client/src/Layouts/Footer.jsx
+++ client/src/Layouts/Footer.jsx
@@ -41,9 +41,9 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2024{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
+            JC™
           </a>
           . All Rights Reserved.
         </span>
