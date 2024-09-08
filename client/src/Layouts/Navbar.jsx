69e126d03d13b3f55f0939882fd3fd84e4d36ca8
Jayant Chugh <108174076+Jayantjc@users.noreply.github.com>
2024-09-08 18:16:10 +0530

    Update Navbar.jsx

--- client/src/Layouts/Navbar.jsx
+++ client/src/Layouts/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
               alt="Flowbite Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Quick Sttore
+              Quick Store
             </span>
           </Link>
 
