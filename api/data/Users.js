4e74edf5c4458dcfbb87512968fb54b70bf67ad9
Jayant Chugh <108174076+Jayantjc@users.noreply.github.com>
2024-10-10 15:54:38 +0530

    Update Users.js

--- api/data/Users.js
+++ api/data/Users.js
@@ -1,16 +1,16 @@
 const bcrypt = require("bcryptjs");
 const users = [
   {
-    name: "Admin",
-    email: "admin@node.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: "abc",
+    email: "abc@gmail.com",
+    password: bcrypt.hashSync("12345678", 10),
     isAdmin: true,
   },
 
   {
     name: "User",
-    email: "user@node.com",
-    password: bcrypt.hashSync("123456", 10),
+    email: "user@gmail.com",
+    password: bcrypt.hashSync("12345678", 10),
   },
 ];
 
