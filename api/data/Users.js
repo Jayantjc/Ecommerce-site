49643bc257542ce7c0886bdc824e34a05b3c43d2
Jayant Chugh <108174076+Jayantjc@users.noreply.github.com>
2024-11-19 22:22:16 +0530

    Update Users.js

--- api/data/Users.js
+++ api/data/Users.js
@@ -3,14 +3,29 @@ const users = [
   {
     name: "Admin",
     email: "admin@node.com",
-    password: bcrypt.hashSync("abc123456", 10),
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
   },
 
   {
     name: "User",
     email: "user@node.com",
-    password: bcrypt.hashSync("abc123456", 10),
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+module.exports = users;
+const users = [
+  {
+    name: "jc",
+    email: "jc@node.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+
+  {
+    name: "User",
+    email: "user@node.com",
+    password: bcrypt.hashSync("123456", 10),
   },
 ];
 module.exports = users;
